c7e32dc22843b2077928b443f0097d043de65df6
Douglas Krieghbaum <Douglas.Krieghbaum@ca.com>
2015-08-18 12:54:27 -0500

    Eclipse Test

--- src/main/java/com/wartree/krieghb/String/Snippets_Main.java
+++ src/main/java/com/wartree/krieghb/String/Snippets_Main.java
@@ -5,8 +5,8 @@ import com.wartree.krieghb.String.String_Basic;;
 public class Snippets_Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//  Basic Sring class usage
 		String_Basic SB_A = new String_Basic();
 
 		SB_A.Print_String();
