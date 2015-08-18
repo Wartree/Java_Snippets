068aad2c989212f8a79bffac17b35991314788fc
Douglas Krieghbaum <Douglas.Krieghbaum@ca.com>
2015-08-18 10:33:42 -0500

    New file

--- src/main/java/com/wartree/krieghb/String/String_Basic.java
+++ src/main/java/com/wartree/krieghb/String/String_Basic.java
@@ -3,7 +3,23 @@ package com.wartree.krieghb.String;
 import java.lang.String;
 
 public class String_Basic {
+	
+	String stringA;
+	
+	public String_Basic() {
+		
+		stringA = "String A";
+		
+		
+	}
 
-	String stringA = "String A";
+	
+	
+	public void Print_String() {
+		
+		System.out.println(stringA); // Display the string.
+		
+	}
+	
 	
 }
