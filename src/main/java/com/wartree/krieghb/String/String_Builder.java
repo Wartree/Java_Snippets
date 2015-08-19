9fdc3713b02211e3ced7129b0f87c5c9d20b451f
Douglas Krieghbaum <Douglas.Krieghbaum@ca.com>
2015-08-18 20:31:39 -0500

    Stuff

--- src/main/java/com/wartree/krieghb/String/String_Builder.java
+++ src/main/java/com/wartree/krieghb/String/String_Builder.java
@@ -9,6 +9,7 @@ public class String_Builder {
 	public String_Builder() {
 		
 		myStringBuilder.insert(0,"Hi There Everybody!");
+		Modify_String(myStringBuilder);
 		
 	}
 	
@@ -17,6 +18,13 @@ public class String_Builder {
 		System.out.println("String Builder:  " + myStringBuilder.toString());
 		
 		System.out.println("Size of String Builder:  " + myStringBuilder.length());
+	}
+	
+	public void Modify_String(StringBuilder changeString) {
+		
+		changeString.replace(0, changeString.length(), "The string was changed");
+		
+		
 	}
 
 }
