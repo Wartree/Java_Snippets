39f4bc4bb1cba706a43b0bb3fd71bf554d33f53d
Douglas Krieghbaum <Douglas.Krieghbaum@ca.com>
2015-08-26 11:10:42 -0500

    space

--- src/main/java/com/wartree/krieghb/utilities/String_Constants.java
+++ src/main/java/com/wartree/krieghb/utilities/String_Constants.java
@@ -2,15 +2,12 @@ package com.wartree.krieghb.utilities;
 
 public final class String_Constants {
 
-	
+
 	public static final String STAR_SEPERATOR = "**************************************************";
     public static final String EOL = "End of Line";
 
 
 
     public static final String END_OF_LINE = "\n\n" + STAR_SEPERATOR + "\n\t\t\t\t" + EOL + "\n" + STAR_SEPERATOR + "\n\n";
-
-
-	
 
 }
